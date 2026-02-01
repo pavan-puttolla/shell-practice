4641636bda878ce622b0b015c47aa806fae997a4
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-02 02:03:17 +0530

    varibles

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1="$1"
+PERSON2="$2"
+echo "$PERSON2:Hi $PERSON1 How are you?"
+echo "$PERSON1:I am fine $PERSON2, How about you?"
+echo "$PERSON2:I am also fine $PERSON1, thanks for asking!"
+echo "$PERSON1:What are your plans for the weekend?"
