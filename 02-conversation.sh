b69769d0e5ecd29779e6e12fa007c496063282b2
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-02 01:38:58 +0530

    varibles actions

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1="Ramesh"
+PERSON2="Suresh"
+echo "$PERSON2:Hi Ramesh How are you?"
+echo "$PERSON1:I am fine $PERSON2, How about you?"
+echo "$PERSON2:I am also fine Ramesh, thanks for asking!"
+echo "$PERSON1:What are your plans for the weekend?"
