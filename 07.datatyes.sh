099ef01ec0352e119159feffad7b069cb4e4db0f
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-02 20:26:14 +0530

    07 data type

--- 07.datatyes.sh
+++ 07.datatyes.sh
@@ -4,3 +4,11 @@ Num1=100
 Num2=200
 sum=$((Num1 + Num2))
 echo "sum of numbers is :$sum "
+
+####Array Data Type###
+frurits=("Pineapple" "Apple" "pomogrinate" "kivi")
+echo" fruits are:${fruits[@]}"
+echo"first fruit:${fruits[0]}"
+echo"second fruit:${fruits[1]}"
+echo"third fruit:${fruits[2]}"
+echo"fourth fruit:${fruits[3]}"
