4c1dd83c95e6a59699cf7b836a1fc9316604ba81
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-02 18:46:53 +0530

    06 special varibles

--- 06-variables.sh
+++ 06-variables.sh
@@ -3,9 +3,9 @@
 echo "All arguments are passed to script:$@"
 echo "number of varibles are passed to script:$#"
 echo "script name is:$0"
-echo " present running directoruy:$pwd"
-echo " is running the script:$user"
-echo "home directory of current user:$home"
+echo " present running directoruy:$PWD"
+echo " is running the script:$USER"
+echo "home directory of current user:$HOME"
 echo "PID of the script is:$$"
 sleep 100 &
 echo "PID of last background process:$!"
