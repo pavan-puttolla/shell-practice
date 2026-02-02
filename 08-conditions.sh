673ae5eedb85b57de9e4d1d06923153a0b739246
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-03 00:16:59 +0530

    08 conditions type

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -9,6 +9,6 @@ if [ $NUMBER -gt 20 ]; then
 echo "given number is :$NUMBER greater than 20"
 elif [ $NUMBER -eq 20 ]; then
 echo "given number is :$NUMBER equal to 20"
-else [ $NUMBER -lt 20 ]; 
+else [ $NUMBER -lt 20 ]; then
 echo "given number is:$NUMBER less than 20"
 fi
