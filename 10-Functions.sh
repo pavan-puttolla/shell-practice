38e3a14e69a22510da4321fce99d89f36c9e18d9
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-05 02:35:53 +0530

    10-functions.sh

--- 10-Functions.sh
+++ 10-Functions.sh
@@ -3,11 +3,11 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
 echo "please run with the root user "
-exit1 
+exit 1 
 fi
 VALIDATE=(){
     if [ $1 -ne 0 ]; then
-echo "$1 installation is failure"
+echo "$2 installation is failure"
 exit 1
 else
 echo "$2 installation is successful"
