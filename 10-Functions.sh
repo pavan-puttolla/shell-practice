df8495765000703d6d1c8780a33c64fb33815a9c
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-06 13:28:34 +0530

    11-log files for functions script

--- 10-Functions.sh
+++ 10-Functions.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
 USERID=$(id -u)
+LOGSFOLDER=/var/log/shell-scriptlogs
+LOGS_FILE=var/log/shell-scriptlogs/$0.log
 if [ $USERID -ne 0 ]; then
 echo "please run with the root user "
 exit 1 
 fi
+mkdir -p $LOGSFOLDER
+
 VALIDATE(){
   if [ $1 -ne 0 ]; then
 echo "$2 installation is failure"
@@ -13,12 +17,12 @@ else
 echo "$2 installation is successful"
 fi
 }
-dnf install nginx -y
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "installing Nginx"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "installing nodejs"
 
-dnf install mysql -y
+dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "installing mysql"
 
