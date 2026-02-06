2e1ebd841009e267b6e25ac550ab99e72914d95b
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-06 13:31:47 +0530

    10-applying functions

---
+++ 11-logs.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+USERID=$(id -u)
+LOGSFOLDER=/var/log/shell-scriptlogs
+LOGS_FILE=var/log/shell-scriptlogs/$0.log
+if [ $USERID -ne 0 ]; then
+echo "please run with the root user "
+exit 1 
+fi
+mkdir -p $LOGSFOLDER
+
+VALIDATE(){
+  if [ $1 -ne 0 ]; then
+echo "$2 installation is failure"
+exit 1
+else
+echo "$2 installation is successful"
+fi
+}
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "installing Nginx"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "installing nodejs"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "installing mysql"
+
