3722ce532e61ce7b2a5cc5d9c1ce00e734be721f
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-07 23:13:30 +0530

    12-logs script using tee command

---
+++ 12-logsusingteecomnd.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+USERID=$(id -u)
+LOGSFOLDER="/var/log/shell-scriptnewlogs"
+LOGS_FILE="$LOGSFOLDER/$0.log"
+if [ $USERID -ne 0 ]; then
+echo "please run with the root user access" | tee -a $LOGS_FILE
+exit 1 
+fi
+mkdir -p $LOGSFOLDER
+
+VALIDATE(){
+  if [ $1 -ne 0 ]; then
+echo "$2 installation is failure" | tee -a $LOGS_FILE
+exit 1
+else
+echo "$2 installation is successful" | tee -a $LOGS_FILE
+fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "installing Nginx"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "installing nodejs"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "installing mysql"
+
