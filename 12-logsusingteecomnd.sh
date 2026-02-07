8f829d2ca98c32e22c5cbc0fccfeaec21d8732cb
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-08 01:52:02 +0530

    12-logs script using tee command

--- 12-logsusingteecomnd.sh
+++ 12-logsusingteecomnd.sh
@@ -5,14 +5,15 @@ LOGS_FOLDER="/var/log/shellsript_logs"
 LOGS_FILE=/var/log/shellsript_logs/$0.log
 if [ $USERID -ne 0 ]; then
 echo " please run with root access " |tee -a $LOGS_FILE 
-exit1
+exit 1
 fi
 mkdir  -p $LOGS_FOLDER
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-    echo "$2 -----installation is failure" |tee -a $LOGS_FILE
+    echo "$2 -----installation is failure" | tee -a $LOGS_FILE
     exit 1
-    else echo$2 ....installation is sucess" |tee -a $LOGS_FILE
+    else 
+    echo"$2 ....installation is sucess" | tee -a $LOGS_FILE
     fi
 }
 
