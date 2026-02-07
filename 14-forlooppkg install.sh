f96e1e1727bce64be8c61600c0e34b742a548c33
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-08 02:47:49 +0530

    14 For loop pakage install

---
+++ 14-forlooppkg install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shellsript_logs"
+LOGS_FILE=/var/log/shellsript_logs/$0.log
+if [ $USERID -ne 0 ]; then
+echo "please run with root access" |tee -a $LOGS_FILE 
+exit 1
+fi
+mkdir  -p $LOGS_FOLDER
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo "$2 -----installation is failure" | tee -a $LOGS_FILE
+    exit 1
+    else 
+    echo "$2 ....installation is sucess" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ #sudo 14-forlooppkg install.sh nginx mysql nodejs
+do
+dnf install $package -y &>> $LOGS_FILE
+VALIDATE $? "Installing $package"
+done
