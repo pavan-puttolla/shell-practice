545c908755b6e7054f785b5a6ca6490538ee3bfe
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-08 01:31:18 +0530

    12-logs script using tee command

--- 12-logsusingteecomnd.sh
+++ 12-logsusingteecomnd.sh
@@ -9,7 +9,7 @@ exit1
 fi
 mkdir  -p $LOGS_FOLDER
 VALIDATE(){
-    if [$1 -ne 0]; then 
+    if [ $1 -ne 0 ]; then
     echo "$2 -----installation is failure" |tee -a $LOGS_FILE
     exit 1
     else  "$2 ....installation is sucess" |tee -a $LOGS_FILE
@@ -18,7 +18,9 @@ VALIDATE(){
 
 dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "installing Nginx"
+
 dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "installing MYSQL"
+
 dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "installing nodejs"
