adb9a6528b207b41a6f5c7af8bc79415fd98fcc9
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-08 00:58:52 +0530

    12-logs script using tee command

--- 12-logsusingteecomnd.sh
+++ 12-logsusingteecomnd.sh
@@ -1,29 +1,24 @@
 #! /bin/bash
 
 USERID=$(id -u)
-LOGSFOLDER="/var/log/shell-scriptnewlogs"
-LOGS_FILE="$LOGSFOLDER/$0.log"
+LOGS_FOLDER="/var/log/shellsript_logs"
+LOGS_FILE=/var/log/shellsript_logs/$0.log
 if [ $USERID -ne 0 ]; then
-echo "please run with the root user access" | tee -a $LOGS_FILE
-exit 1 
+echo " please run with root access " |tee -a $LOGS_FILE 
+exit1
 fi
-mkdir -p $LOGSFOLDER
-
+mkdir  -p $LOGS_FOLDER
 VALIDATE(){
-  if [ $1 -ne 0 ]; then
-echo "$2 installation is failure" | tee -a $LOGS_FILE
-exit 1
-else
-echo "$2 installation is successful" | tee -a $LOGS_FILE
-fi
+    if [$1 -ne 0]; then 
+    echo "$2 -----installation is failure " |tee -a $LOGS_FILE
+    exit 1
+    else  "$2 ....installation is sucess" |tee -a $LOGS_FILE
+    fi
 }
 
 dnf install nginx -y &>> $LOGS_FILE
-VALIDATE $? "installing Nginx"
-
+VALIDATE $? "installing Nginx
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "installing MYSQL"
 dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "installing nodejs"
-
-dnf install mysql -y &>> $LOGS_FILE
-VALIDATE $? "installing mysql"
-
