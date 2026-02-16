fc1029d88ccb1f75662e4620598ebdca4f2151e1
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-16 17:17:24 +0530

    colors

---
+++ 17.colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+Y="\e[33m" # Yellow color
+B="\e[34m" # Blue color
+M="\e[35m" # Magenta color
+C="\e[36m" # Cyan color
+W="\e[37m" # White color
+N="\e[0m"  # No color (reset)
+
+
+echo -e "$R I am learning  Linux $N, $G and I am enjoying it $N"
