f6712fa224f5c4e099c9b353116ef22f0e168bd3
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-16 16:26:08 +0530

    package validation with exitcodes

--- 16-ski[ppingwithexitcode.sh
+++ 16-ski[ppingwithexitcode.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shellsriptforlooppkg_logs"
 LOGS_FILE=/var/log/shellsriptforlooppkg_logs/$0.log
 if [ $USERID -ne 0 ]; then
-echo "please run with root access" |tee -a $LOGS_FILE 
+echo "please run with root access" | tee -a $LOGS_FILE 
 exit 1
 fi
 mkdir  -p $LOGS_FOLDER
@@ -19,10 +19,11 @@ VALIDATE(){
 
 for package in $@ # sudo 14-forlooppkginstall.sh nginx mysql nodejs
 do
-dnf list installed $package -y &>> $LOGS_FILE
-echo "checking $package is already installed or not" | tee -a $LOGS_FILE
-if [ $? -ne 0 ]; then
-VALIDATE $? "Installing $package"
+    dnf list installed $package  &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+    echo "$package not installed,installing now" | tee -a $LOGS_FILE
+    dnf install $package -y &>> $LOGS_FILE
+    VALIDATE $? " $package installation"
 else
 echo "$package is already installed,skipping " | tee -a $LOGS_FILE
 fi      
