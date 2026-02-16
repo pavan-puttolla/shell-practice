2665188e5c6d16d1a30089c1850ec891255e5fe4
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-16 19:49:12 +0530

    set and trap

--- 20-trap.sh
+++ 20-trap.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-set -e #ERR
 
+set -e #ERR
 trap "echo 'this is error in line $LINENO,command $BASH_COMMAND'" ERR
 
-echo "hello world"
-echo "this is shell script for trap command"
-eecho "this is second line"
-echo0 "this is third line"
-echo "this is fourth line"
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shellsriptforlooppkg_logs"
+LOGS_FILE=/var/log/shellsriptforlooppkg_logs/$0.log
+if [ $USERID -ne 0 ]; then
+echo -e "$R please run with root access $N" | tee -a $LOGS_FILE 
+exit 1
+fi
+mkdir  -p $LOGS_FOLDER
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo -e "$2 -----installation is $R failure $N" | tee -a $LOGS_FILE
+    exit 1
+    else 
+    echo -e "$2 ....installation is $G success $N" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo 14-forlooppkginstall.sh nginx mysql nodejs
+do
+    dnf list installed $package  &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+    echo "$package not installed,installing now" | tee -a $LOGS_FILE
+    dnf install $package -y &>> $LOGS_FILE
+    VALIDATE $? " $package installation"
+else
+echo -e "$Y $package is already installed,skipping $N" | tee -a $LOGS_FILE
+fi      
+done    
